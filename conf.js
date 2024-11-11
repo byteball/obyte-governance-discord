5c191a92883bd98fd0df4a8f24c7bcaec74d699b
Taump <taump@yandex.ru>
2024-11-11 14:47:15 +0400

    fix: remove extra conf

--- conf.js
+++ conf.js
@@ -7,7 +7,6 @@ exports.bNoPassphrase = true;
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.logoUrl = 'https://avatars.githubusercontent.com/u/20998761?s=200&v=4';
 
-exports.SERVER_ID = process.env.SERVER_ID;
 exports.CHANNEL_ID = process.env.CHANNEL_ID;
 exports.BOT_TOKEN = process.env.BOT_TOKEN;
 
